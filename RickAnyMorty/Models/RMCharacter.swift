f0f011d827e3865445950b100e6d532fd54133d1
gorkem <grkm.tn@gmail.com>
2023-01-10 23:23:01 +0300

    sa

--- RickAnyMorty/Models/RMCharacter.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RMCharacter.swift
-//  RickAnyMorty
-//
-//  Created by Görkem Tüneri on 1/10/23.
-//
-
-import Foundation
--- RickAnyMorty/Models/RMEpisode.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RMEpisode.swift
-//  RickAnyMorty
-//
-//  Created by Görkem Tüneri on 1/10/23.
-//
-
-import Foundation
